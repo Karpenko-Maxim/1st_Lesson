6b7bb758562dd8f8ab4130b6287356c9b65748ec
Karpenko-Maxim <112941666+Karpenko-Maxim@users.noreply.github.com>
2022-09-10 18:19:02 +0300

    Add files via upload

---
+++ Lab.0/Lab.0/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// ex.1
+double weight = 52.2;
+double height = 1.7;
+double BMI = weight / (height*height);
+Console.WriteLine($"My BMI is {BMI}");
+ //ex.2
+int a = 100 % 13;
+int b = 17 - (a % 13);
+double grad = 13 * 100;
+double pi = 3.1415;
+double sec = (grad * 1) / 360;
+double l = 2 * pi * Math.Sqrt((Math.Pow(a, 2) + Math.Pow(b, 2)) / 2) / sec;
+Console.WriteLine(l);
+// ex.3 - недостаточно входных данных
+
+
+
+
+
+
+
+
+
